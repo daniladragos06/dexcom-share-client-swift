f6339c05894ca49686cf4714132c7cf2c3d5e9e2
daniladragos06 <61890245+daniladragos06@users.noreply.github.com>
2020-03-07 16:43:18 +0200

    Update ShareClient.swift

--- ShareClient/ShareClient.swift
+++ ShareClient/ShareClient.swift
@@ -41,7 +41,7 @@ public enum KnownShareServers: String {
          https://github.com/dabear/NightscoutShareServer
     */
 
-     case CUSTOM="https://dragos06.herokuapp.com"
+     case CUSTOM="https://nightscoutshareserver4caf.azurewebsites.net"
 }
 
 // From the Dexcom Share iOS app, via @bewest and @shanselman:
